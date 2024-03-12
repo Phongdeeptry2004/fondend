83ce1b41c3ae2596c63ec236b1ab710dfcfe43ab
Phongdao <kanata2003232@gmail.com>
2024-03-12 19:41:39 +0700

    12_3

---
+++ test/js/controllers.js
@@ -0,0 +1,27 @@
+app.controller('TitleController', function ($scope, $location) {
+    var urlSttring = $location.absUrl();
+    var url = new URL(urlSttring);
+    var path1 = url.pathname;
+    var path = path1.replace(/\.[^/.]+$/, "");
+    console.log("Path: " + path);
+    switch (path) {
+        case '/test/index':
+            $scope.pageTitle = 'Trang Chủ';
+            break;
+        case '/test/dangnhap':
+            $scope.pageTitle = 'Đăng Nhập';
+            break;
+        case '/test/dangky':
+            $scope.pageTitle = 'Đăng Ký';
+            break;
+        case '/test/contact':
+            $scope.pageTitle = 'Liên Hệ';
+            break;
+        case '/test/booking':
+            $scope.pageTitle = 'Đặt Khoá Học';
+            break;
+        default:
+            $scope.pageTitle = 'Không tồn tại';
+            break;
+    }
+});
